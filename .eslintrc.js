5492945425fbced1a03c4dbfa6ce41e293c3977a
Vincent Girard <vinccool96@gmail.com>
2022-08-16 13:17:31 -0400

    Style settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,40 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  root: true,
+  extends: ["eslint:recommended", "prettier"],
   globals: {},
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: 'module'
+    sourceType: "module"
   },
-  plugins: ['@typescript-eslint'],
-  ignorePatterns: ['lib/*'],
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["lib/*"],
   rules: {
-    'no-cond-assign': [2, 'except-parens'],
-    'no-unused-vars': 0,
-    'no-redeclare': 0,
-    '@typescript-eslint/no-unused-vars': 1,
-    'no-empty': [
-      'error',
+    "no-cond-assign": [2, "except-parens"],
+    "no-unused-vars": 0,
+    "no-redeclare": 0,
+    "@typescript-eslint/no-unused-vars": 1,
+    "no-empty": [
+      "error",
       {
         allowEmptyCatch: true
       }
     ],
-    'prefer-const': [
-      'warn',
+    "prefer-const": [
+      "warn",
       {
-        destructuring: 'all'
+        destructuring: "all"
       }
     ],
-    'spaced-comment': 'warn'
+    "spaced-comment": "warn",
+    curly: "error",
+    quotes: [
+      "error",
+      "double",
+      { avoidEscape: true, allowTemplateLiterals: false }
+    ],
+    "quote-props": ["error", "as-needed"]
   }
 }
