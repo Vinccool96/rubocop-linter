70f4f0c2bbc928fa0af064a4b924ba2c71922f15
Vincent Girard <vinccool96@gmail.com>
2022-08-18 11:10:06 -0400

    More debug info

--- src/git.ts
+++ src/git.ts
@@ -5,12 +5,18 @@ const octokit = new Octokit({ auth: getInput("github_token") })
 
 export async function getDiffFiles(commit: string): Promise<string[]> {
   const gitWorkspace = process.env.GITHUB_WORKSPACE!
+  debug(gitWorkspace, "gitWorkspace")
   const fullRepo = process.env.GITHUB_ACTION_REPOSITORY!.split("/")
+  debug(fullRepo, "fullRepo")
   const owner = fullRepo[0]
+  debug(owner, "owner")
   const repo = fullRepo[1]
+  debug(repo, "repo")
   const files: Set<string> = new Set()
   const commitData = (await octokit.rest.repos.getCommit({ owner, repo, ref: commit }).then((c) => c)).data
+  debug(commitData, "commitData")
   for (const file of commitData.files!) {
+    debug(file, "file")
     if (file.status !== "removed") {
       files.add(`${gitWorkspace}/${file.filename}`)
     }
